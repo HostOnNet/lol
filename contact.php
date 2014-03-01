d59bae6c1cff3a33bd24f301dda423905dffef02
vikay69 <vkaytsanov@abv.bg>
2014-03-01 22:47:21 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,24 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-<html lang="bg" xml:lang="bg" xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-    <title>Getboosted.com | The place to get higher </title>
-    <meta http-equiv="Content-Type" content="text/html charset=uft-8" />
-    <meta name="keywords" content="boosting, lol, booster, league of legends, league, legends, of" />
-    <meta name="googlebot" content="index, follow" />
-    <meta name="description" content="Here you can get boosted in the game League of Legends" />
-    <meta name="author" content="BurnToBeEviL, RealistCC" />
-    <meta name="design" content="RealistCC" />
-    <meta name="code" content="BurnToBeEviL" />
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-    <link type="text/css" rel="stylesheet" href="css/style.css" />
-    <script src="http://code.jquery.com/jquery-1.9.0.js"></script>
-    <script src="js/lol.js"></script>
-
-
-    </head>
-
-		<body>
+<? require 'header.php'; ?>
             <div id="head">
                 <a href="index.php"><div id="logo"></div></a>
                 <div class="rightsidehead">
@@ -64,3 +44,6 @@ function print_secure_content()
     Comments <br><textarea name="comments" style="width:100%;height:120px;margin:0 0 10px;"></textarea>
     <input type="submit" value="Send">
 </form>
+
+</body>
+</html>
